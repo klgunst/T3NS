01e568d1355957ded6f2648b577c011a09f307a0
Klaas Gunst <Klaas.Gunst@UGent.be>
2018-12-10 19:11:28 +0100

    Changed io hdf5

--- include/io_to_disk.h
+++ include/io_to_disk.h
@@ -17,11 +17,9 @@ void read_from_disk(const char filename[], struct siteTensor ** const T3NS,
 void write_dataset(hid_t id, const char datname[], const void * dat, 
                    hsize_t size, enum hdf5type kind);
 
-void read_dataset(hid_t id, const char datname[], void * dat, 
-                  enum hdf5type kind);
+void read_dataset(hid_t id, const char datname[], void * dat);
 
 void write_attribute(hid_t group_id, const char atrname[], const void * atr, 
                      hsize_t size, enum hdf5type kind);
 
-void read_attribute(hid_t id, const char atrname[], void * atr, 
-                    enum hdf5type kind);
+void read_attribute(hid_t id, const char atrname[], void * atr);
